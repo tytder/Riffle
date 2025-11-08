f1f52bdcb1964aaa89f9d81462c37b9d8ef01687
tytder <matiswardenier@gmail.com>
2025-11-08 23:11:11 +0100

    Create IndexToNumberConverter.cs

---
+++ Player.Windows/Converters/IndexToNumberConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Riffle.Player.Windows.Converters;
+
+public class IndexToNumberConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is int index)
+            return (index + 1).ToString();
+        return "1";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => Binding.DoNothing;
+}
